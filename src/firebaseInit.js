5f598a43f2048caeb845373b4d93570c190d8b20
Aman Panchal <amanpanchal2753@gmail.com>
2024-08-12 12:51:57 +0530

    .env Updation

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmn0WUjdKE6CqZ8g17zJJ9G7b7PYchsPc",
-  authDomain: "photoalbum-8409b.firebaseapp.com",
-  projectId: "photoalbum-8409b",
-  storageBucket: "photoalbum-8409b.appspot.com",
-  messagingSenderId: "759017016387",
-  appId: "1:759017016387:web:2030feb168d2b8f615d8f6"
+  apiKey:process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBAS_EAPP_ID
 };
 
 // Initialize Firebase
